56e9836969f670d026ecb6e0281c3f6fe57331b1
Shawn Taylor <shawntaylor@weecology.org>
2018-12-11 14:40:56 -0500

    put nice columns in admin interface

--- main/admin.py
+++ main/admin.py
@@ -3,7 +3,18 @@ from django.contrib import admin
 # Register your models here.
 from .models import IssueDates, Speciess, Phenophases, Forecasts
 
-admin.site.register(Forecasts)
-admin.site.register(Speciess)
-admin.site.register(Phenophases)
-admin.site.register(IssueDates)
+@admin.register(IssueDates)
+class IssueDatesAdmin(admin.ModelAdmin):
+    list_display = ('issue_date','display_text')
+    
+@admin.register(Speciess)
+class SpeciessAdmin(admin.ModelAdmin):
+    list_display = ('species', 'display_text','default')
+
+@admin.register(Phenophases)
+class PhenophasesAdmin(admin.ModelAdmin):
+    list_display = ('phenophase', 'display_text','default')
+
+@admin.register(Forecasts)
+class ForecastsAdmin(admin.ModelAdmin):
+    list_display = ('issue_date','phenophase','species')
